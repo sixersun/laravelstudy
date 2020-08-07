982d6a13e12c8f844a1bb921715ab1214bf3962c
夏韧旭 <xia@niaoni.la>
2020-08-07 18:02:06 +0800

    显示头像

--- storage/framework/views/d2d87f2ad99af4317333a9977898846696077018.php
+++ storage/framework/views/d2d87f2ad99af4317333a9977898846696077018.php
@@ -23,8 +23,8 @@
         <?php else: ?>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
-            <?php echo e(Auth::user()->name); ?>
+              <img src="<?php echo e(Auth::user()->avatar); ?>" class="img-responsive img-circle" width="30px" height="30px">
+              <?php echo e(Auth::user()->name); ?>
 
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
