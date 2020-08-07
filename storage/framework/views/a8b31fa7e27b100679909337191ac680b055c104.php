20c45008a201790ae5bf25ea4fe2dc889433e691
夏韧旭 <xia@niaoni.la>
2020-08-07 13:10:40 +0800

    基础页面结构

---
+++ storage/framework/views/a8b31fa7e27b100679909337191ac680b055c104.php
@@ -0,0 +1,6 @@
+<?php $__env->startSection('title', '首页'); ?>
+
+<?php $__env->startSection('content'); ?>
+  <h1>这里是首页</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Volumes/Work/home/smartclick/blog/Laravel/resources/views/pages/root.blade.php ENDPATH**/ ?>
