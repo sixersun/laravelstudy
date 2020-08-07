8f586b885a6aaf9bc89dc7de1c11c963cc69a4a8
夏韧旭 <xia@niaoni.la>
2020-08-07 16:38:57 +0800

    邮箱认证

---
+++ storage/framework/views/4baec1a03d2bad44a388bf1944356651e0e0dd4e.php
@@ -0,0 +1,2 @@
+[<?php echo e($slot); ?>](<?php echo e($url); ?>)
+<?php /**PATH /Volumes/Work/home/smartclick/blog/Laravel/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b63a9ea7ed80933cbf42c048681b3855144514d1.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH /Volumes/Work/home/smartclick/blog/Laravel/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
