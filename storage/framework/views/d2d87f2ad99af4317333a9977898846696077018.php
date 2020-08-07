0b63672b458a9710a0368ceb405636e8a169f303
夏韧旭 <xia@niaoni.la>
2020-08-07 14:02:22 +0800

    修复跳转链接

--- storage/framework/views/d2d87f2ad99af4317333a9977898846696077018.php
+++ storage/framework/views/d2d87f2ad99af4317333a9977898846696077018.php
@@ -1,25 +1,47 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
-  <div class="container">
+<div class="container">
     <!-- Branding Image -->
     <a class="navbar-brand " href="<?php echo e(url('/')); ?>">
-      LaraBBS
+    LaraBBS
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- Left Side Of Navbar -->
-      <ul class="navbar-nav mr-auto">
+    <!-- Left Side Of Navbar -->
+    <ul class="navbar-nav mr-auto">
 
-      </ul>
+    </ul>
 
-      <!-- Right Side Of Navbar -->
-      <ul class="navbar-nav navbar-right">
+    <!-- Right Side Of Navbar -->
+    <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
+        <?php if(auth()->guard()->guest()): ?>
         <li class="nav-item"><a class="nav-link" href="<?php echo e(route('login')); ?>">登录</a></li>
         <li class="nav-item"><a class="nav-link" href="<?php echo e(route('register')); ?>">注册</a></li>
-      </ul>
+        <?php else: ?>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+            <?php echo e(Auth::user()->name); ?>
+
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="">个人中心</a>
+            <a class="dropdown-item" href="">编辑资料</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" id="logout" href="#">
+                <form action="<?php echo e(route('logout')); ?>" method="POST">
+                <?php echo e(csrf_field()); ?>
+
+                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                </form>
+            </a>
+            </div>
+        </li>
+        <?php endif; ?>
+    </ul>
     </div>
-  </div>
+</div>
 </nav><?php /**PATH /Volumes/Work/home/smartclick/blog/Laravel/resources/views/layouts/_header.blade.php ENDPATH**/ ?>
