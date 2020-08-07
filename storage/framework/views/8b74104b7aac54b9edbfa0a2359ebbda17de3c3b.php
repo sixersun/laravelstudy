7199a91ef4ce0b161609ad02794bc42954c86c3a
夏韧旭 <xia@niaoni.la>
2020-08-07 18:00:21 +0800

    上传头像

--- storage/framework/views/8b74104b7aac54b9edbfa0a2359ebbda17de3c3b.php
+++ storage/framework/views/8b74104b7aac54b9edbfa0a2359ebbda17de3c3b.php
@@ -12,7 +12,9 @@
 
       <div class="card-body">
 
-        <form action="<?php echo e(route('users.update', $user->id)); ?>" method="POST" accept-charset="UTF-8">
+      <form action="<?php echo e(route('users.update', $user->id)); ?>" method="POST" 
+          accept-charset="UTF-8" 
+          enctype="multipart/form-data">
           <input type="hidden" name="_method" value="PUT">
           <input type="hidden" name="_token" value="<?php echo e(csrf_token()); ?>">
           <?php echo $__env->make('shared._error', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
@@ -27,6 +29,16 @@
           <div class="form-group">
             <label for="introduction-field">个人简介</label>
             <textarea name="introduction" id="introduction-field" class="form-control" rows="3"><?php echo e(old('introduction', $user->introduction)); ?></textarea>
+          </div>
+
+          <div class="form-group mb-4">
+            <label for="" class="avatar-label">用户头像</label>
+            <input type="file" name="avatar" class="form-control-file">
+
+            <?php if($user->avatar): ?>
+              <br>
+              <img class="thumbnail img-responsive" src="<?php echo e($user->avatar); ?>" width="200" />
+            <?php endif; ?>
           </div>
           <div class="well well-sm">
             <button type="submit" class="btn btn-primary">保存</button>
